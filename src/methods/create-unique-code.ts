4ffc70152010376ab3606549b6c65f941984ae89
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2023-06-28 01:03:08 +0300

    Updates

--- src/methods/create-unique-code.ts
+++ src/methods/create-unique-code.ts
@@ -1,11 +1,11 @@
-export let createUniqueCode = (obj: any): string => {
-    let uniqueCode: string = ``;
-    for (const i in obj) {
-        uniqueCode += `${obj[i].toString()}-`;
-    }
-    let lastIndex = uniqueCode.lastIndexOf("-");
-    if (lastIndex !== -1 && lastIndex === uniqueCode.length - 1) {
-        uniqueCode = uniqueCode.slice(0, lastIndex);
-    }
+export const createUniqueCode = (obj: any): string => {
+    let uniqueCode: string = `xxx`;
+    // for (const i in obj) {
+    //     uniqueCode += `${obj[i].toString()}-`;
+    // }
+    // let lastIndex = uniqueCode.lastIndexOf("-");
+    // if (lastIndex !== -1 && lastIndex === uniqueCode.length - 1) {
+    //     uniqueCode = uniqueCode.slice(0, lastIndex);
+    // }
     return uniqueCode;
 }
