32b4d5e00e69d449b3ca303c4efc7e8c807ca8a3
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2023-06-27 03:55:53 +0300

    Updates

---
+++ src/methods/core.ts
@@ -0,0 +1,11 @@
+export const createUniqueCode = (obj: any) => {
+    let uniqueCode = ``;
+    for (const i in obj) {
+        uniqueCode += `${obj[i]}-`;
+    }
+    let lastIndex = uniqueCode.lastIndexOf("-");
+    if (lastIndex !== -1 && lastIndex === uniqueCode.length - 1) {
+        uniqueCode = uniqueCode.slice(0, lastIndex);
+    }
+    return uniqueCode;
+}
