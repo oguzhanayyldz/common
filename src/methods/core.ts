2e54cedc09c0040b49621cdb7216a711a6f6cf25
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2023-06-28 20:04:04 +0300

    Updates

--- src/methods/core.ts
+++ src/methods/core.ts
@@ -1,4 +1,4 @@
-export function createUniqueCode (obj: any): string {
+export const createUniqueCode = (obj: any): string  => {
     let uniqueCode: string = ``;
     for (const i in obj) {
         uniqueCode += `${obj[i].toString()}-`;
@@ -8,4 +8,14 @@ export function createUniqueCode (obj: any): string {
         uniqueCode = uniqueCode.slice(0, lastIndex);
     }
     return uniqueCode;
+}
+
+export const generateRandomString = (length: number): string => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
+    }
+    return randomString;
 }
