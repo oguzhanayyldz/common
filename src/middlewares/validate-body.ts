93a4d8e3d805b67884d7d76548a98299f122dc7d
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2024-02-12 21:57:51 +0300

    Updates

--- src/middlewares/validate-body.ts
+++ src/middlewares/validate-body.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { Schema, checkSchema, validationResult } from 'express-validator';
+import { Schema, ValidationError, checkSchema, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 
 
@@ -20,7 +20,9 @@ export const validateBody = (schema: Schema) => {
             // Tanımlanmayan alanları kontrol et
             const extraFields = Object.keys(req.body).filter((field) => !Object.keys(schema).includes(field));
             if (extraFields.length > 0) {
-                return res.status(400).json({ error: 'Tanımlanmayan alanlar: ' + extraFields.join(', ') });
+                // return res.status(400).json({ error: 'Tanımlanmayan alanlar: ' + extraFields.join(', ') });
+                let validationError: any = [{ msg: `Tanımlanmayan alanlar: ${extraFields.join(', ')}` }];
+                throw new RequestValidationError (validationError);
             }
 
             // Her şey geçerliyse, sonraki middleware'e geç
