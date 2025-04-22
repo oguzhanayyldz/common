5091c2d6752652be81fbc81c516497b795cb4bd0
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2025-04-22 05:48:46 +0300

    Updates

--- src/core/integration-credentials.ts
+++ src/core/integration-credentials.ts
@@ -1,8 +1,8 @@
 import { ResourceName } from "../types/resourceName";
 
-export interface IntegrationCredentials {
-    user: string | undefined;
-    platform: ResourceName | undefined;
-    integration_active: boolean | undefined;
-    [key: string]: any;// Diğer platform-özel kimlik bilgilerine izin ver
+export class IntegrationCredentials {
+    public user: string | undefined;
+    public platform: ResourceName | undefined;
+    public integration_active: boolean | undefined;
+    [key: string]: any;
 }
