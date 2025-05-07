c3a9257a379f2134de34ccfb8dc7c4d40e641e95
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2025-05-07 19:02:05 +0300

    Updates

--- src/events/relation-product-link-created-event.ts
+++ src/events/relation-product-link-created-event.ts
@@ -3,31 +3,25 @@ import { ProductType } from "./types/product-type";
 
 export interface RelationProductLinkCreatedEvent {
     subject: Subjects.RelationProductLinkCreated;
-    data: RelationProductLinkCreatedEventData;
-}
-
-export interface RelationProductLinkCreatedEventData {
-    list: RelationProductLinkCreatedEvent[];
-}
-
-export interface RelationProductLinkCreatedEventDataListItem {
-    id: string;
-    uuid: string;
-    user: string;
-    version: number;
-    product: {
+    data: {
         id: string;
         uuid: string;
         user: string;
         version: number;
-        type: ProductType;
+        product: {
+            id: string;
+            uuid: string;
+            user: string;
+            version: number;
+            type: ProductType;
+        };
+        relatedProduct: {
+            id: string;
+            uuid: string;
+            user: string;
+            version: number;
+            type: ProductType;
+        };
+        uniqueCode?: string | null;
     };
-    relatedProduct: {
-        id: string;
-        uuid: string;
-        user: string;
-        version: number;
-        type: ProductType;
-    };
-    uniqueCode?: string | null;
 }
--- src/events/relation-product-link-updated-event.ts
+++ src/events/relation-product-link-updated-event.ts
@@ -3,33 +3,27 @@ import { ProductType } from "./types/product-type";
 
 export interface RelationProductLinkUpdatedEvent {
     subject: Subjects.RelationProductLinkUpdated;
-    data: RelationProductLinkUpdatedEventData
-}
-
-export interface RelationProductLinkUpdatedEventData {
-    list: RelationProductLinkUpdatedEvent[];
-}
-
-export interface RelationProductLinkUpdatedEventDataListItem {
-    id: string;
-    uuid: string;
-    user: string;
-    version: number;
-    deleted?: boolean;
-    deletionDate?: Date | null;
-    product: {
+    data: {
         id: string;
         uuid: string;
         user: string;
         version: number;
-        type: ProductType;
-    };
-    relatedProduct: {
-        id: string;
-        uuid: string;
-        user: string;
-        version: number;
-        type: ProductType;
-    };
-    uniqueCode?: string | null;
+        deleted?: boolean;
+        deletionDate?: Date | null;
+        product: {
+            id: string;
+            uuid: string;
+            user: string;
+            version: number;
+            type: ProductType;
+        };
+        relatedProduct: {
+            id: string;
+            uuid: string;
+            user: string;
+            version: number;
+            type: ProductType;
+        };
+        uniqueCode?: string | null;
+    }
 }
