f6d8a220477f05855aed616e09b048704d81eccb
Oğuzhan Ayyıldız <oguzhanayyldz@hotmail.com>
2025-05-11 04:51:05 +0300

    Updates

--- src/interfaces/product-image-integration-updated.interface.ts
+++ src/interfaces/product-image-integration-updated.interface.ts
@@ -3,7 +3,7 @@ import { ProductIntegrationImage } from "./product-integration-created.interface
 export interface ProductImageIntegrationUpdated {
     sku: string;
     barcode?: string;
+    images: ProductIntegrationImage[];
     combinationId?: string;
     productId?: string;
-    url?: ProductIntegrationImage[];
 }
